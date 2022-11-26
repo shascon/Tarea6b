1853d99366a0cd4634107bd712bc7bcfe683ed56
shascon <106252001+shascon@users.noreply.github.com>
2022-11-26 12:30:48 -0300

    xx

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const pokemonContenedor = document.querySelector(".pokemon");
 
 
-//Función con fetch para api
+
 
 const fetchPokemon = (id)=> {
     try{
@@ -22,7 +22,7 @@ const fetchPokemon = (id)=> {
 }
 
 
-// Función reccorre con for ocupando limit
+
 const personaje = () =>{
   fetchpersonajes = []
   for (let i = 1; i <= 20; i++) {
@@ -34,35 +34,35 @@ const personaje = () =>{
 
 const NewPokemon = (pokemon) => {
 
-// Se crea div de carta llamado Carta
+
   const carta = document.createElement("div");
   carta.classList.add("carta");
 
-// Se crea contenedor de la carta pokemosn
+
   const cartacontenedor = document.createElement("div");
   cartacontenedor.classList.add("carta-contendor");
 
-// Nodo de refrrencia carta
+
   carta.appendChild(cartacontenedor);
 
-// Se crea el div para mostrar personaje donde contendra los datos
+
   const personaje = document.createElement("div");
   personaje.classList.add("personaje-block");
 
-// Se crea div con donde contendrá la imagen 
+
   const imgcontenedor = document.createElement("div");
   imgcontenedor.classList.add("img-container");
 
-// Se crea variable para traer imagen de URL y posicionar en eqiqueta img src
+
 
   const imegen = document.createElement("img");
   imegen.src = pokemon.sprites.front_default;
 
-// Nodo de refrrencia de la imegen
 
-imgcontenedor.appendChild(imegen);
 
-// Se crea etiqueta p para refrencia nombre  
+  imgcontenedor.appendChild(imegen);
+
+
 
   const nombre = document.createElement("p");
   nombre.classList.add("nombre");
